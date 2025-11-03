2f4a54864da1c1bd1b42a8f19f85e32bbd61401a
bebofcds <bebo.boby3@gmail.com>
2025-11-03 17:52:57 +0200

    a commit

--- hoserof_api/fbinitialization.go
+++ hoserof_api/fbinitialization.go
@@ -7,56 +7,59 @@ import (
 	"cloud.google.com/go/firestore"
 	"golang.org/x/crypto/bcrypt"
 )
-var studentsInFirestore=client.Collection("students")
+
+var studentsInFirestore = client.Collection("students")
+
 func CheckPasswordHash(password, hash string) bool {
-    err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
-    return err == nil
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
-func loadStudentDocument(s string)*firestore.DocumentRef{
+func loadStudentDocument(s string) *firestore.DocumentRef {
 	return studentsInFirestore.Doc(s)
 }
-func studentLoginToFirestore(user User)(string,*UserData,error){
+func studentLoginToFirestore(user User) (*UserData, error) {
 	fmt.Println("function called")
-	studentDocument:=loadStudentDocument(user.StudentId)
-	if studentDocument==nil{
+	studentDocument := loadStudentDocument(user.StudentId)
+	if studentDocument == nil {
 		fmt.Println("user not found")
-		return "",nil,errors.New("user not found")
+		return nil, errors.New("user not found")
 	}
-	studentDocumentAsMap,err:=studentDocument.Get(ctx)
-	if err!=nil{
+	studentDocumentAsMap, err := studentDocument.Get(ctx)
+	if err != nil {
 		fmt.Println("failed to map user")
-		return "",nil,errors.New("failed to map user")
+		return nil, errors.New("failed to map user")
 	}
 	var userInFirestore UserInFirestore
-	if err:=studentDocumentAsMap.DataTo(&userInFirestore);err!=nil{
+	if err := studentDocumentAsMap.DataTo(&userInFirestore); err != nil {
 		fmt.Println("failed to assign user to struct")
-		return "",nil,errors.New("failed to assign user to struct")
+		return nil, errors.New("failed to assign user to struct")
 	}
-	var (studentToken string; 
-		err1 error;
-		userData UserData;)
-		fmt.Println(user.StudentPassword,userInFirestore.FirestoreStudentPassword)
-	if user.StudentId==userInFirestore.FirestoreStudentId&&CheckPasswordHash(user.StudentPassword,userInFirestore.FirestoreStudentPassword){
-		userData=mapUserDataToResponse(userInFirestore)
+	var (
+		studentToken string
+		err1         error
+		userData     UserData
+	)
+	fmt.Println(user.StudentPassword, userInFirestore.FirestoreStudentPassword)
+	if user.StudentId == userInFirestore.FirestoreStudentId && CheckPasswordHash(user.StudentPassword, userInFirestore.FirestoreStudentPassword) {
+		userData = mapUserDataToResponse(userInFirestore)
 		fmt.Println("user verified")
-		studentToken,err1=jwtGenerator(user.StudentId,userData.StudentClass,userData.StudentName)
-		if err1!=nil{
+		studentToken, err1 = jwtGenerator(user.StudentId, userData.StudentClass, userData.StudentName)
+		if err1 != nil {
 			fmt.Println("failed to return token")
-			return "",nil,errors.New("failed to return token")
+			return nil, errors.New("failed to return token")
 		}
-	} else{
+	} else {
 		fmt.Println("user access denied. invalid id/password")
 	}
 	fmt.Println("successfully performed operations")
-	
-	return studentToken,&userData,nil
+	userData.StudentToken = studentToken
+	return &userData, nil
 }
-func mapUserDataToResponse(f UserInFirestore) UserData{
+func mapUserDataToResponse(f UserInFirestore) UserData {
 	return UserData{
-		StudentId:f.FirestoreStudentId,
-		StudentName: f.FirestoreStudentName,
+		StudentId:    f.FirestoreStudentId,
+		StudentName:  f.FirestoreStudentName,
 		StudentClass: f.FirestoreStudentClass,
-	
 	}
 
 }
--- hoserof_api/signup.go
+++ hoserof_api/signup.go
@@ -7,46 +7,49 @@ import (
 	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/bcrypt"
 )
-type NewUser struct{
-	NewStudentID string `json:"student_id"`
-	NewStudentName string `json:"student_name"`
-	NewStudentPhoneNumber uint64 `json:"student_phonenumber"`
-	NewStudentPassword []byte `json:"student_password"`
-	NewStudentAge uint8 `json:"student_age"`
-	NewStudentGrade uint8 `json:"student_grade"`
-	NewStudentClass string `json:"student_class"`
+
+type NewUser struct {
+	NewStudentID          string `json:"student_id"`
+	NewStudentName        string `json:"student_name"`
+	NewStudentPhoneNumber string `json:"student_phonenumber"`
+	NewStudentPassword    string `json:"student_password"`
+	NewStudentAge         string `json:"student_age"`
+	NewStudentGrade       string `json:"student_grade"`
+	NewStudentClass       string `json:"student_class"`
 }
-func signup(c *gin.Context){
+
+func signup(c *gin.Context) {
 	fmt.Println("signup request made")
 	var newUser NewUser
-	if err:=c.ShouldBindJSON(&newUser);err!=nil{
-		c.JSON(http.StatusBadRequest,gin.H{"error":"couldn't format json"})
+	if err := c.ShouldBindJSON(&newUser); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "couldn't format json"})
 	}
-	hashedPassword, err := bcrypt.GenerateFromPassword(newUser.NewStudentPassword, bcrypt.DefaultCost)
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(newUser.NewStudentPassword), bcrypt.DefaultCost)
 	if err != nil {
-    fmt.Println(err)
+		fmt.Println(err)
 	}
-	newUser.NewStudentPassword=hashedPassword
-	signedUp:=signUpToFirestore(newUser)
-	if !signedUp{
-		c.JSON(http.StatusBadRequest,gin.H{"error":"failed to sign up"})
-	}else {
-		c.JSON(http.StatusOK,gin.H{"success":"signed up"})
+	newUser.NewStudentPassword = string(hashedPassword)
+	signedUp := signUpToFirestore(newUser)
+	if !signedUp {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "failed to sign up"})
+	} else {
+		c.JSON(http.StatusOK, gin.H{"success": "signed up"})
 	}
 }
-func signUpToFirestore(n NewUser)bool{
-	_,err:=client.Collection("students").Doc(n.NewStudentID).Set(ctx,map[string]interface{}{
-		"student_name":n.NewStudentName,
-		"student_id":n.NewStudentID,
-		"student_phonenumber":n.NewStudentPhoneNumber,
-		"student_password":n.NewStudentPassword,
-		"student_age":n.NewStudentAge,
-		"student_grade":n.NewStudentGrade,
-		"student_class":n.NewStudentClass,
+func signUpToFirestore(n NewUser) bool {
+	_, err := client.Collection("students").Doc(n.NewStudentID).Set(ctx, map[string]interface{}{
+		"student_name":        n.NewStudentName,
+		"student_id":          n.NewStudentID,
+		"student_phonenumber": n.NewStudentPhoneNumber,
+		"student_password":    n.NewStudentPassword,
+		"student_age":         n.NewStudentAge,
+		"student_grade":       n.NewStudentGrade,
+		"student_class":       n.NewStudentClass,
 	})
-	if err!=nil{
+	if err != nil {
 		fmt.Println("failed to signup to firestore")
+		fmt.Println(err)
 	}
-	
+
 	return true
 }
