a7aa34813404237e213ec65a50b0a0a61b29e52d
Fady Samy <47241432+iTzFady@users.noreply.github.com>
2025-12-02 23:59:08 +0200

    Initialize Firebase with environment variable support
    Add support for Firebase credentials from environment variable or file

--- config/firebase.go
+++ config/firebase.go
@@ -3,6 +3,7 @@ package config
 import (
 	"context"
 	"log"
+	"os"
 
 	"cloud.google.com/go/firestore"
 	firebase "firebase.google.com/go"
@@ -16,7 +17,14 @@ var opt option.ClientOption
 
 func InitFirebase() {
 	ctx = context.Background()
-	opt = option.WithCredentialsFile("./hoserof_fb.json")
+	if firebaseCredsJSON := os.Getenv("FIREBASE_CREDENTIALS_JSON"); firebaseCredsJSON != "" {
+		log.Println("Initializing Firebase from environment variable...")
+		opt = option.WithCredentialsJSON([]byte(firebaseCredsJSON))
+	} else {
+		log.Println("Initializing Firebase from credentials file...")
+		opt = option.WithCredentialsFile("./hoserof_fb.json")
+	}
+
 	app, err := firebase.NewApp(ctx, nil, opt)
 	if err != nil {
 		log.Fatalf("Firebase: Error Occured: %v", err)
